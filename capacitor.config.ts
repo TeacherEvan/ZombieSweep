513179f5b8d21c1ab76bb67d8d2b9bb120ed76e8
TeacherEvan <ewiebotha@gmail.com>
2026-08-03 17:24:45 +0700

    chore: capacitor native build config + dependency bump

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.ewaldt.zombiesweep',
+  appName: 'ZombieSweep',
+  webDir: 'dist',
+};
+
+export default config;
